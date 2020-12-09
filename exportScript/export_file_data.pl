26f70a177d7434f78aafae863ee5c26b2837e025
nihuaqing <nihuaqing@beyondsoft.com>
2020-12-09 22:15:01 +0800

    AutoCommit

--- exportScript/export_file_data.pl
+++ exportScript/export_file_data.pl
@@ -1,15 +1,15 @@
 User=root
 Password=123456
 #数据文件绝对路径
-DataFile=/data/export/123
+DataFile=/data/export/list
 #数据文件分隔符
 DataDelimiter=/
 #编码格式 界面未维护该值
-DataFileFormat=/data/export/123_format
+DataFileFormat=data_format
 #SchemaName名
 SchemaName=etl_tool
 #表名
-TableName=ftp_script_info9
+TableName=ftp_script_info
 
 #执行数据库加载脚本
 mysql -u${root} -p${Password} -e "select * from ${SchemaName}.${TableName} into outfile '${DataFile}' Fields TERMINATED by '${DataDelimiter}'"
